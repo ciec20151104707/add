628330e069859107d58c10925c5fbe3f00f35e2f
liyi <s20151104707@mac24.local>
2017-02-27 10:13:25 +0800

    Initial Commit

---
+++ 累加/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  累加
+//
+//  Created by s20151104707 on 17/2/27.
+//  Copyright © 2017年 s20151104707. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
